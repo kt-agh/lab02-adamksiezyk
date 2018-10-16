694d852d3d457afaba4218f2a61a3f2f82d2dbe3
Adam Ksiezyk <ksiezyk@adam.com>
2018-10-16 15:43:43 +0200

    C++ programs

--- Lab2.cc
+++ Lab2.cc
@@ -7,8 +7,8 @@ using namespace std;
 
 int main() {
 	int PierwszaZmienna=5; int Drugazmienna=7;
-	int WynikDziałania=PierwszaZmienna+Drugazmienna;
+	int WynikDzialania=PierwszaZmienna+Drugazmienna;
 
-	cout <<"Wynik Dodawania Dwóch Liczb To:"<<WynikDziałania<<endl; // Wypisuje wynik dzialania
+    cout <<"Wynik Dodawania Dwoch Liczb To:"<<WynikDzialania<<endl; // Wypisuje wynik dzialania
 	return 0;
 }
